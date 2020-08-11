375adb874716cf7e6f46d620102643f81eca6af3
Kunye Song <1811868@nzse.ac.nz>
2020-08-12 11:04:06 +1200

    Register create,API create.

---
+++ ICT638June2020Group2API/Models/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICT638June2020Group2API.Models
+{
+    public class Register
+    {
+
+        public int id { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string phonenumber { get; set; }
+        public string address { get; set; }
+        public string country { get; set; }
+        public string uniquename { get; set; }
+        public string password { get; set; }
+
+    }
+}
