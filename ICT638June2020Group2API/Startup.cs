84d5e61d98696b2a9022e01f914edf00f2f738cd
Kunye Song <1811868@nzse.ac.nz>
2020-08-12 11:55:32 +1200

    Controller

--- ICT638June2020Group2API/Startup.cs
+++ ICT638June2020Group2API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ICT638June2020Group2API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ICT638June2020Group2API
 {
@@ -25,6 +27,9 @@ namespace ICT638June2020Group2API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RegisterContext>(opt =>
+               opt.UseInMemoryDatabase("RegisterList"));
+
             services.AddControllers();
         }
 
