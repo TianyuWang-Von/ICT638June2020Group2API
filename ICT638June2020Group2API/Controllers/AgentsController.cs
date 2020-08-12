ce11e0055f2afa7f4743eead86dd6b805b248fdc
Administrator <Administrator@DESKTOP-EU336QI>
2020-08-12 12:47:06 +1200

    Agent Api create

---
+++ ICT638June2020Group2API/Controllers/AgentsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ICT638June2020Group2API.Models;
+
+namespace ICT638June2020Group2API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        private readonly AgentContext _context;
+
+        public AgentsController(AgentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Agents
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agent>>> GetAgents()
+        {
+            return await _context.Agents.ToListAsync();
+        }
+
+        // GET: api/Agents/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Agent>> GetAgent(long id)
+        {
+            var agent = await _context.Agents.FindAsync(id);
+
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            return agent;
+        }
+
+        // PUT: api/Agents/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAgent(long id, Agent agent)
+        {
+            if (id != agent.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(agent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AgentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Agents
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Agent>> PostAgent(Agent agent)
+        {
+            _context.Agents.Add(agent);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAgent", new { id = agent.Id }, agent);
+        }
+
+        // DELETE: api/Agents/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Agent>> DeleteAgent(long id)
+        {
+            var agent = await _context.Agents.FindAsync(id);
+            if (agent == null)
+            {
+                return NotFound();
+            }
+
+            _context.Agents.Remove(agent);
+            await _context.SaveChangesAsync();
+
+            return agent;
+        }
+
+        private bool AgentExists(long id)
+        {
+            return _context.Agents.Any(e => e.Id == id);
+        }
+    }
+}
